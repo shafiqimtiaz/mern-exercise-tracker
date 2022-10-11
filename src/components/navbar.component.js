2467ae66f55fda5ecfe0ead522bfd5ebdce012a4
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2022-10-11 12:59:44 -0400

    add navbar

---
+++ src/components/navbar.component.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.png";
+
+export default class Navbar extends Component {
+	render() {
+		return (
+			<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+				<Link to="/" className="navbar-brand">
+					<img src={logo} alt="logo" width="30" height="30" className="d-inline-block align-top mx-2" />
+					Ex-Tracker
+				</Link>
+				<div className="collpase navbar-collapse">
+					<ul className="navbar-nav mr-auto">
+						<li className="navbar-item">
+							<Link to="/" className="nav-link">
+								Exercises
+							</Link>
+						</li>
+						<li className="navbar-item">
+							<Link to="/create" className="nav-link">
+								Create Exercise
+							</Link>
+						</li>
+						<li className="navbar-item">
+							<Link to="/user" className="nav-link">
+								Create User
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		);
+	}
+}
