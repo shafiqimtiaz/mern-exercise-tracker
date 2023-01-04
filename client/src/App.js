21ae15130787efe7cd30bd613405ce9c08c29ded
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-01-03 19:21:02 -0500

    update file name

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Navbar from "./components/navbar.component";
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
+import Navbar from "./components/navbar";
+import ExercisesList from "./components/exercises-list";
+import EditExercise from "./components/edit-exercise";
+import CreateExercise from "./components/create-exercise";
+import CreateUser from "./components/create-user";
 
 function App() {
 	return (
